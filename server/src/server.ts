fd25e1db9e6219cb34892799940d3602dee71640
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2025-03-04 16:41:43 +0200

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,6 +10,11 @@ const PORT = process.env.PORT ?? 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API is working!" });
+});
+
 app.use("/api/recipes", recipeRoutes);
 
 app.listen(PORT, () => {
