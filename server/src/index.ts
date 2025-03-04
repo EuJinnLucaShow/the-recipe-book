5f4104c6079d9a87cd1becf25b55951e12cb29f5
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2025-03-04 16:47:01 +0200

    Created index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,12 +10,11 @@ const PORT = process.env.PORT ?? 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/recipes", recipeRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "API is working!" });
 });
-
-app.use("/api/recipes", recipeRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
